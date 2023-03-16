c0297a68ab1c0b70b841c58f5d074382352ae35d
umbcstudent <125592850+umbcstudent@users.noreply.github.com>
2023-03-16 23:47:53 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,40 @@
+# This function returns the entirety of "Dracula" as a string
+def readBook():
+  f = open("dracula.txt", "r")
+  s = f.read().replace("-", " ")
+  f.close()
+  return ''.join(c for c in s if c.isalnum() or c == " ")
+#This function analyze the string to calculate the variable total counts
+def summarize_book():
+    # Read the book
+    text = readBook()
+    # Get word count and find most common word
+    words = text.lower().split()
+    word_count = {}
+    for word in words:
+        if word in word_count:
+            word_count[word] += 1
+        else:
+            word_count[word] = 1
+    most_common_word = max(word_count, key=word_count.get)
+    most_common_word_count = word_count[most_common_word]
+
+    # Get unique four-letter words
+    unique_four_letter_words = set(word for word in words if len(word) == 4)
+
+    # Get words that appear more than 500 times
+    frequent_words = {word: count for word, count in word_count.items() if count > 500}
+
+    # Display results
+    print("'{}' is the word that appear the most throughout a text, the total of {} times".format(most_common_word, most_common_word_count))
+    print("There are {} words that are four letter long".format(len(unique_four_letter_words)))
+    print("I noticed that these words show up more than 500 times:")
+    for word, count in frequent_words.items():
+        print("{} - {}".format(word, count))
+      
+#Call the function
+summarize_book()
+
+
+
+
